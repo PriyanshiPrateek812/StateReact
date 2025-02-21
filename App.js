3b6e246697f1343e86542ee43150a1f502a2e648
PriyanshiPrateek812 <154115675+PriyanshiPrateek812@users.noreply.github.com>
2025-02-21 09:18:09 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import Header from "./components/ClassState";
+
+import Header from "./components/FunctionState2";
+
+// import Header from "./components/FunctionState";
+
+
+function App() {
+  
+  return (
+    <div className="container">
+      {/* <Header /> */}
+      <Header />
+    </div>
+  );
+}
+
+export default App;
